ab805d6fe5117939cc6443500705c2c4fe608656
krishnadas3 <krishnadasp004@gmail.com>
2023-11-13 17:36:05 +0530

    tindog

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// var obj={
+//     name="hey",
+    
+// }
+
+let obj ={
+
+    name:"mnvivek",
+    age:"81",
+    student:"yes"
+}
+
+let name= obj.name
+let age=obj.age
+let student=obj.student
+
+console.log(name);
+console.log(age)
+console.log(student);
